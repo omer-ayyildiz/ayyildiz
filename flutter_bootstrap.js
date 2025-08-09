c3b709fa4917b431e21dd1e770ab1d8f9f56898d
omer-ayyildiz <100423349+omer-ayyildiz@users.noreply.github.com>
2025-08-09 16:58:04 +0000

    Deploying to gh-pages from @ omer-ayyildiz/ayyildiz@973e198de4d964e69cfdb7bab6fe1d21ab6839c7 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"52cc75c62467e72200ab5cd54cee48b8313f6d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1236795836"
+    serviceWorkerVersion: "3095195034"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "324b467db2ab21f01e8c27db46df742c",
+const RESOURCES = {"flutter_bootstrap.js": "92b03b0bc2dcf302585eb48ad8a3bba5",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
